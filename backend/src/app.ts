61f97c39b174099a127e1bb3cb3725b7d3cb4b06
Davidvera27 <davidrestrepovera@gmail.com>
2025-01-07 23:38:24 -0500

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,20 +3,23 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import caseRentsRoutes from './routes/caseRentsRoutes';
 import protocolistRentsRoutes from './routes/protocolistRentsRoutes';
+import emailRoutes from './routes/emailRoutes';
 
 const app = express();
 const PORT = 5000;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// Rutas
 app.use('/api', caseRentsRoutes);
 app.use('/api', protocolistRentsRoutes);
+app.use('/api', emailRoutes);
 
+// Servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
