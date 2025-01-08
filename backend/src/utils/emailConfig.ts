b1f7bcbd2be9d9ea584d30568f110abcb92e671f
Davidvera27 <davidrestrepovera@gmail.com>
2025-01-07 23:38:32 -0500

    Create emailConfig.ts

---
+++ backend/src/utils/emailConfig.ts
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+
+// Configuración del transportador SMTP
+export const transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false, // No usamos conexión segura (SSL/TLS)
+  auth: {
+    user: "drestrepo@correo.iue.edu.co",
+    pass: "vera.96082719544",
+  },
+});
+
+// Verificar conexión con el servidor SMTP
+transporter.verify((error) => {
+  if (error) {
+    console.error("Error al conectar con el servidor SMTP:", error);
+  } else {
+    console.log("Servidor SMTP listo para enviar correos.");
+  }
+});
