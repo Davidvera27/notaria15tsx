997a62ab9a25e3e6bd6e48b68cc3d518fa3e1413
Davidvera27 <davidrestrepovera@gmail.com>
2025-01-08 15:09:47 -0500

    Update emailConfig.ts

--- backend/src/utils/emailConfig.ts
+++ backend/src/utils/emailConfig.ts
@@ -2,14 +2,15 @@ import nodemailer from "nodemailer";
 
 // Configuración del transportador SMTP
 export const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
-  port: 587,
-  secure: false, // No usamos conexión segura (SSL/TLS)
-  auth: {
-    user: "drestrepo@correo.iue.edu.co",
-    pass: "vera.96082719544",
-  },
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: "drestrepo@correo.iue.edu.co",
+        pass: "vera.96082719544",
+    },
 });
+
 
 // Verificar conexión con el servidor SMTP
 transporter.verify((error) => {
